e0aae1c599bd68997d13d8c92e5382a6fdef1547
iwaliner <iwaliner.railway@gmail.com>
2024-05-02 18:34:10 +0900

    fixed a bug on Emitter

--- src/main/java/com/iwaliner/urushi/block/EulaliaBlock.java
+++ src/main/java/com/iwaliner/urushi/block/EulaliaBlock.java
@@ -1,9 +1,13 @@
 package com.iwaliner.urushi.block;
 
 import com.iwaliner.urushi.ItemAndBlockRegister;
+import com.iwaliner.urushi.util.UrushiUtils;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.DoublePlantBlock;
@@ -12,9 +16,15 @@ import net.minecraft.world.level.block.state.BlockState;
 
 import net.minecraft.util.RandomSource;
 
+import java.util.List;
+
 public class EulaliaBlock extends TallGrassBlock {
     public EulaliaBlock(Properties p_57318_) {
         super(p_57318_);
+    }
+    @Override
+    public void appendHoverText(ItemStack p_49816_, @org.jetbrains.annotations.Nullable BlockGetter p_49817_, List<Component> list, TooltipFlag p_49819_) {
+        UrushiUtils.setInfo(list,"small_flower");
     }
     public void performBonemeal(ServerLevel p_57320_, RandomSource p_57321_, BlockPos p_57322_, BlockState p_57323_) {
         DoublePlantBlock doubleplantblock = (DoublePlantBlock) ItemAndBlockRegister.double_eulalia.get();
